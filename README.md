832c1fc36f23e34f1290295c6127f8082636d344
Paul <rainpaul365@gmail.com>
2024-09-11 09:08:11 +0200

    Adding get_next_line

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # get_next_line
-42 get next line
+## 42 get next line
+
+## Get Next Line
+This is my second exercise at 42 school.
+This part of the program will take a file descriptor (int) as an argument. Each time the function is called, it will return the next line from the file associated with that file descriptor. When an error occurs or when all lines have been read, get_next_line will free its buffer and return NULL.
+
+## Prototype:
+char	*get_next_line(int fd);
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pudry <pudry@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 09:04:08 by pudry             #+#    #+#             */
+/*   Updated: 2024/09/11 09:04:08 by pudry            ###   ########.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+int		ft_strchr(char *ptr);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *str);
+char	*ft_get_ptr_line(char *ptr, char *file, int fd);
+char	*ft_give_line(char *str);
+char	*ft_remove_line(char *ptr);
+char	*get_next_line(int fd);
+
+#endif
